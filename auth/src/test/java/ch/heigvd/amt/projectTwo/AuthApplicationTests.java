060f84a516efc5fd2f61db5eb5481b190fcd0cc7
Mickael Bonjour <mickael.bonjour@heig-vd.ch>
2019-12-02 15:49:19 +0100

    Added auth endpoint init

---
+++ auth/src/test/java/ch/heigvd/amt/projectTwo/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.heigvd.amt.projectTwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
