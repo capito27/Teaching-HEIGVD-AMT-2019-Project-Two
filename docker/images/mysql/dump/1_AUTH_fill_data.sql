d44687966b8ea7b0ff84392384c827b25946869c
Filipe Fortunato <filipe.fortunato@heig-vd.ch>
2020-01-04 18:31:57 +0100

    change DB creation

--- docker/images/mysql/dump/1_AUTH_fill_data.sql
+++ docker/images/mysql/dump/1_AUTH_fill_data.sql
@@ -1,10 +1,10 @@
 use `amt_auth`;
 
 INSERT INTO `user` (`firstname`, `lastname`, `password`, `email`,`is_admin`) VALUES 
-("user", "name", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", "user@mail.co",0),
+("admin", "istrator", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", "admin@mail.co",1),
 ("filipe", "fortunato", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", "filipe@mail.co",0),
+("jonathan", "zaehringer", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", "john@mail.co",0),
 ("mickael", "bonjour", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", "mickael@mail.co",0),
 ("pierre", "kohler", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", "pete@mail.co",0),
-("jonathan", "zaehringer", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", "john@mail.co",0),
-("admin", "istrator", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", "admin@mail.co",1);
+("user", "name", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", "user@mail.co",0);
 
